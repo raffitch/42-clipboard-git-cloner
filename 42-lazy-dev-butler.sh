1e6d99174c4d4bbddec058613d4339c01ba0a012
Raffi Tchakerian <16430301+raffitch@users.noreply.github.com>
2024-12-06 12:39:33 +0400

    Update 42-lazy-dev-butler.sh

--- 42-lazy-dev-butler.sh
+++ 42-lazy-dev-butler.sh
@@ -10,10 +10,13 @@
 #                                                                              #
 # **************************************************************************** #
 
+# Repository: https://github.com/raffitch/42-lazy-dev-butler.git
+
 # Lazy Dev's Clipboard Butler: 
 # Why do boring manual work when you can automate it?
 # This script listens to your clipboard for Git URLs, clones them, opens them in VS Code,
 # and even cleans up after itself when you're done. All you need to do is sit back, copy, and quit!
+# Give the script permission to run and you're set to go!: chmod +x lazy-dev-butler.sh
 
 #!/bin/bash
 
